98effc9d892375afd20eae220dbfa884ba3709a5
Yasir Khan <yasirkhanone22@gmail.com>
2021-03-22 18:02:11 +0500

    git init

---
+++ project1.py
@@ -0,0 +1,9 @@
+# simple program for finding even and odd number:
+
+number = int(input('Enter you Number'))
+division = number / 2
+
+if division == 0:
+    print('You entered an even number')
+else:
+    print('You entered an odd number')
