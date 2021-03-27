81903e846b79c8c78abbfee3ece7c192e620ba66
Yasir Khan <yasirkhanone22@gmail.com>
2021-03-27 19:01:49 +0500

    Rolling Dice added

---
+++ rolling_dice.py
@@ -0,0 +1,18 @@
+<bold> This is a classic "roll the dice" program. 
+We will be using the random module for this,since we want to randomize the numberswe get from the dice.<bold>
+
+import random
+
+mini = 1
+maxi = 6
+
+rolling = 'Yes'
+
+while rolling == "Yes" or rolling == 'y':
+    print('Rolling the dice..')
+    print('\tHere are the values: ')
+    print(random.randint(mini, maxi))
+    print(random.randint(mini, maxi))
+    break
+
+
