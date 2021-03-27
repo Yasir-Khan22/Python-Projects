54fdebc8950b7c1144aa48b4a583feacd45ec2b5
yasirkhancyber <63096083+yasirkhancyber@users.noreply.github.com>
2021-03-27 19:05:01 +0500

    Update rolling_dice.py

--- rolling_dice.py
+++ rolling_dice.py
@@ -1,5 +1,4 @@
-<bold> This is a classic "roll the dice" program. 
-We will be using the random module for this,since we want to randomize the numberswe get from the dice.<bold>
+<bold>This is a classic roll the dice program We will be using the random module for this, since we want to randomize the numbers we get from the dice.<bold>
 
 import random
 
